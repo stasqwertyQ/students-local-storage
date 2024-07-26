68a57fcec90606ff51c0ac12f91506d7b293f39b
stasqwertyQ <125751548+stasqwertyQ@users.noreply.github.com>
2024-07-26 17:17:18 +0300

    dz

---
+++ index.js
@@ -0,0 +1,30 @@
+const forELemrent = document.getElementById("studentForm");
+const Tableref = document.getElementById("studentsTable").querySelector("tbody"); 
+
+Tableref.addEventListener("submit", OnSubmit);
+
+function OnSubmit(event) {
+    event.preventDefault();
+
+
+    const form = event.currentTarget;
+
+    const students = {
+        id: Data.now
+        firstName:
+        lastName:
+        age:
+        course:
+        faculty:
+            querySelector();
+    }
+
+
+
+
+
+    event.reset();
+
+}
+
+
